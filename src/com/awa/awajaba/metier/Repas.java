dc29753a511db1918087819bb417e823d6a7dc0e
ZIEMNIAK <leonard.ziemniak.sio@gmail.com>
2019-10-08 16:53:59 +0200

    implémentation des methodes get et set de numero de la classe Repas

--- src/com/awa/awajaba/metier/Repas.java
+++ src/com/awa/awajaba/metier/Repas.java
@@ -18,5 +18,12 @@ public class Repas {
         this.numero = numero;
         this.date = date;
     }
+    
+    public int getNumero(){
+        return this.numero;
+    }
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
   
 }
