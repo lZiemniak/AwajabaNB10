8f7e79eed8c584d340d82cbf6b3004b496fcd52e
ZIEMNIAK <leonard.ziemniak.sio@gmail.com>
2019-10-08 16:50:55 +0200

    Implémentation du constructeur de Repas

--- src/com/awa/awajaba/metier/Repas.java
+++ src/com/awa/awajaba/metier/Repas.java
@@ -14,5 +14,9 @@ public class Repas {
     private int numero;
     private LocalDate date;
     
+    public Repas(int numero, LocalDate date){
+        this.numero = numero;
+        this.date = date;
+    }
   
 }
